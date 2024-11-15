76e49b81c0e5a82a1c38a4f0f5506a8045ba0cad
bortoliero <gabortoliero@gmail.com>
2024-11-15 12:15:46 -0300

    cadastro de usuarios bd

--- Mobile/invest_control/lib/services/api_service.dart
+++ Mobile/invest_control/lib/services/api_service.dart
@@ -2,26 +2,42 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  final String baseUrl = 'http://localhost:3000'; // Altere para o URL real do back-end
+  final String baseUrl = 'http://192.168.100.49:3000'; // Altere para o URL real do back-end
 
   Future<bool> registerUser(String name, String email, String password) async {
     final url = Uri.parse('$baseUrl/users'); // Substitua pela rota correta no NestJS
 
-    final response = await http.post(
-      url,
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'name': name,
-        'email': email,
-        'password': password,
-      }),
-    );
+    try {
+      // Log dos dados enviados
+      print('Enviando dados para cadastro:');
+      print('Name: $name');
+      print('Email: $email');
+      print('Password: $password');
+      print('url: $url');
 
-    if (response.statusCode == 201) {
-      return true; // Sucesso
-    } else {
-      print('Erro: ${response.body}');
-      return false; // Falha
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'name': name,
+          'email': email,
+          'password': password,
+        }),
+      );
+
+      if (response.statusCode == 201) {
+        // Sucesso
+        print('Usuário registrado com sucesso');
+        return true;
+      } else {
+        // Erro: mostra o código de status e o corpo da resposta
+        print('Erro: ${response.statusCode}, ${response.body}');
+        return false;
+      }
+    } catch (e) {
+      // Captura de erros de rede ou outros
+      print('Erro ao conectar com o servidor: $e');
+      return false;
     }
   }
 }
--- Mobile/invest_control/test/widget_test.dart
+++ Mobile/invest_control/test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:invest_control/main.dart';
 
 void main() {
