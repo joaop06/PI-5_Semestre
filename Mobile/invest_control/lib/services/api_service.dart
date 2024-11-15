24937a35644389fef976196e395c451e238c2a02
bortoliero <gabortoliero@gmail.com>
2024-11-15 12:21:19 -0300

    ajuste URL

--- Mobile/invest_control/lib/services/api_service.dart
+++ Mobile/invest_control/lib/services/api_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  final String baseUrl = 'http://192.168.100.49:3000'; // Altere para o URL real do back-end
+  final String baseUrl = 'http://localhost:3000'; // Altere para o URL real do back-end
 
   Future<bool> registerUser(String name, String email, String password) async {
     final url = Uri.parse('$baseUrl/users'); // Substitua pela rota correta no NestJS
